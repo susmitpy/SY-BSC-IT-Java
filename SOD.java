7d64a6699e3bd5ceb1a88e936ef584e3fdbef26d
Susmit <susmit.py@gmail.com>
2019-12-17 18:19:30 +0530

    methoda

--- SOD.java
+++ SOD.java
@@ -5,23 +5,16 @@ class SOD
 {
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter length of array: ");
-		int n = sc.nextInt();
-		char[] arr = new char[n];
-		System.out.format("Enter %d elements: ", n);
-		sc.nextLine();
-		for(int i = 0; i<n; i++){
-			String s = sc.nextLine(); 
-			arr[i] = s.charAt(0);
-		}
-		
+		System.out.println("Enter String: ");
+		String s = sc.nextLine();
+		char[] arr = s.toCharArray();
 		System.out.println("Sum Of Digits: " + getSum(arr));
 	}
 
         static int getSum(char[] arr){
 		int sum=0;
 		for (char c : arr){
-			sum += Integer.parseInt(String.valueOf(c)); 
+			sum += Character.getNumericValue(c);
 		}
 		return sum;
 	}
